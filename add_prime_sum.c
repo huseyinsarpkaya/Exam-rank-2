fbf55b0e7d6c2df9c61f0600cdd6940a111344a6
huseyinsarpkaya <sarpkaya.hsyn@gmail.com>
2025-01-24 12:07:09 +0300

    Update add_prime_sum.c

--- add_prime_sum.c
+++ add_prime_sum.c
@@ -4,7 +4,7 @@ int ft_atoi(char *str)
     int i = 0;
     int res= 0;
     int sign = 1;
-    while(str[i] == 32 && (str[i] >= 9 && str[i] <= 13))
+    while(str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
         i++;
     if (str[i] == '-' || str[i] == '+')
     {
